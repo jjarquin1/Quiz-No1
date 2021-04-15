fcc63a99e408f6f80f5321d4eba2959fbf2b7b26
jkooda <77827825+jjarquin1@users.noreply.github.com>
2021-04-14 23:54:09 -0700

    created a for loop on javacript to cycle through the questions. will be adding statements to check the answers and a result box to view and save high scores.

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 var startBtn = document.querySelector(".start-button");
-var Quiz_box = document.querySelector(".Quiz-box")
+var Quiz_box1 = document.querySelector(".Quiz-box1");
+var Quiz_box2 = document.querySelector(".Quiz-box2")
+var Quiz_box3 = document.querySelector(".Quiz-box3")
 var seconds = document.querySelector(".seconds");
-var timerEl = 60;
+var Question= document.querySelector(".Question");
+var choice_list = document.querySelector(".choice-list");
+var choice = document.querySelector(".choice");
+var options = document.querySelectorAll(".option");
+var onQuestion = 1;
+var timer = 60;
 
-console.log("hello")
 
-function showQuestions() {
-    var Questions = document.querySelector(".Question");
-    Quiz_box.style.display="block";
+function startQuiz() {
+    Quiz_box1.style.display="block";
     startBtn.style.display="none";
     setTime();
-    
 }
+
 
 function setTime() { 
     var timerInterval = setInterval(function() {
-        timerEl--;
-        seconds.textContent = timerEl + " until quiz is over.";
-        if(timerEl === 0) {
+        timer--;
+        seconds.textContent = timer + " seconds left";
+        if(timer === 0) {
             clearInterval(timerInterval);
         }
       }, 1000);
     }
 
-    startBtn.addEventListener("click", showQuestions); 
+    startBtn.addEventListener("click", startQuiz); 
+
+    for (i=0; i<options.length; i++) {
+        options[i].addEventListener("click",function(event){
+            var userchoice = event.target.getAttribute("choice");
+            onQuestion++;
+            if (onQuestion===2){
+                Quiz_box1.style.display="none"
+                Quiz_box2.style.display="block"
+            }
+            else if (onQuestion===3) {
+                Quiz_box2.style.display="none"
+                Quiz_box3.style.display="block"
+            }
+        })
+    }
 
 
 
